ae7183e22a1648bff16b77768c73a022a134e861
Oussama Jarrousse <oussama@jarrousse.org>
2022-07-13 00:38:12 +0200

    improved settings for letsencrypt

--- setup.sh
+++ setup.sh
@@ -11,6 +11,10 @@ docker-compose -f ./docker-compose-first.yaml up nginx -d
 docker-compose -f ./docker-compose-first.yaml up certbot
 docker-compose -f ./docker-compose-first.yaml down
 
+# some configurations for let's encrypt
+curl -L --create-dirs -o etc/letsencrypt/options-ssl-nginx.conf https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf
+openssl dhparam -out etc/letsencrypt/ssl-dhparams.pem 2048
+
 crontab ./etc/crontab
 
 docker-compose -f ./docker-compose.yaml up -d
