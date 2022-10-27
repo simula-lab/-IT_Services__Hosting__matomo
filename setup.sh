71863746d0edb1486b4bb9aebd983696b273c3e6
Oussama Jarrousse <oussama@jarrousse.org>
2022-10-28 00:57:52 +0200

    fixed crontab and added cron_job.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-#!/bin/bash
 echo MARIADB_USER=matomo > .env
 echo MARIADB_PASSWORD=`openssl rand 30 | base64 -w 0` >> .env
 echo MARIADB_ROOT_PASSWORD=`openssl rand 30 | base64 -w 0` >> .env
